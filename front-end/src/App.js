c718ba9a0abba6b80de71e90ab380f95e4218aa3
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2022-07-26 15:31:44 +0300

    menu animation done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,7 +10,8 @@ import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
 import RoundButton from './components/RoundButton/index';
 import { Colors } from './colors';
-
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
@@ -29,12 +30,34 @@ const MyRoutes = function() {
 }
 
 
-function showThemeMenu() {
-
-}
-
-
 function App() {
+
+
+  const [menuStyle, setMenuStyle] = useState ({
+    right: "-100%"
+  }); 
+
+  const [btnsStyle, setBtnsStyle] = useState ({
+    right: "50px"
+  })
+
+  const [visible, setVisible] = useState(false);
+
+
+  const toggleThemeMenu = () => {
+
+    if (visible) {
+      setBtnsStyle({right: "50px"});
+      setMenuStyle({right: "-300px"});
+      setVisible(false);
+    }
+    else {
+      setBtnsStyle({right: "250px"});
+      setMenuStyle({right: "25px"});
+      setVisible(true);
+    }
+  }
+
   return (
     <BrowserRouter>
       <div className="wrapper">
@@ -47,27 +70,31 @@ function App() {
         </div>
 
 
-        <div className="menu">
-          <RoundButton icon={ faCog } onClick={ showThemeMenu } />
-          <RoundButton icon={ faMoon }  />
+        <div className="menu" >
 
-          <div className="theme-menu">
+        <div className="round-btn" style={ btnsStyle } onClick={ toggleThemeMenu }>
+            <FontAwesomeIcon icon={ faCog } />
+        </div>
+
+        <div className="round-btn rbtn-2" style={ btnsStyle }>
+            <FontAwesomeIcon icon={ faMoon } />
+        </div>
+
+          <div className="theme-menu" style={ menuStyle }>
             <div className="theme-menu-text">
               Theme colors
             </div>
             <div className="theme-menu-colors">
+              
               <div style={{backgroundColor: Colors.orange}}>
-
               </div>
               <div style={{backgroundColor: Colors.red}}>
-                
               </div>
               <div style={{backgroundColor: Colors.green}}>
-                
               </div>
               <div style={{backgroundColor: Colors.blue}}> 
-                
               </div>
+
             </div>
           </div>
         </div>
